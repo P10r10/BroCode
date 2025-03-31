04e0f5bd77fb72ec33b3b8aab36dd66b45f18155
P10r10 <asantiago@keemail.me>
2025-03-31 15:34:49 +0100

    updated README.md

--- bro-code/src/App.jsx
+++ bro-code/src/App.jsx
@@ -1,11 +1,20 @@
-import UserGreeting from "./components/UserGreeting/UserGreeting.jsx";
+import Card from "./components/Card/Card.jsx";
+import christina from "./assets/christina.jpg"
+import bruno from "./assets/bruno.jpg"
+import miley from "./assets/miley.jpg"
+import weeknd from "./assets/weeknd.jpg"
+import lady from "./assets/lady.jpg"
 
 
 function App() {
 
     return (
         <>
-            <UserGreeting user="Alexandre" isLogged={true}/>
+            <Card foto={christina} name="Cristina Aguilera" bio="Eu gosto de aprender!"/>
+            <Card foto={bruno} name="Bruno Mars" bio="Eu gosto de aprender!"/>
+            <Card foto={miley} name="Miley Cirus" bio="Eu gosto de aprender!"/>
+            <Card foto={weeknd} name="The Weeknd" bio="Eu gosto de aprender!"/>
+            <Card foto={lady} name="Lady Gaga" bio="Eu gosto de aprender!"/>
         </>
     );
 
@@ -19,8 +28,8 @@ export default App
 // import miley from "./assets/miley.jpg"
 // import weeknd from "./assets/weeknd.jpg"
 // import lady from "./assets/lady.jpg"
-            // <Card foto={christina} name="Cristina Aguilera" bio="Eu gosto de aprender!"/>
-            // <Card foto={bruno} name="Bruno Mars" bio="Eu gosto de aprender!"/>
-            // <Card foto={miley} name="Miley Cirus" bio="Eu gosto de aprender!"/>
-            // <Card foto={weeknd} name="The Weeknd" bio="Eu gosto de aprender!"/>
-            // <Card foto={lady} name="Lady Gaga" bio="Eu gosto de aprender!"/>
+// <Card foto={christina} name="Cristina Aguilera" bio="Eu gosto de aprender!"/>
+// <Card foto={bruno} name="Bruno Mars" bio="Eu gosto de aprender!"/>
+// <Card foto={miley} name="Miley Cirus" bio="Eu gosto de aprender!"/>
+// <Card foto={weeknd} name="The Weeknd" bio="Eu gosto de aprender!"/>
+// <Card foto={lady} name="Lady Gaga" bio="Eu gosto de aprender!"/>
